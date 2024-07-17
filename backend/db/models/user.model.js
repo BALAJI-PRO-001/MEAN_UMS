d50d13530c185e74c796f96c814e240a7dd41532
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-17 21:09:25 +0530

    Feat: getUserByEmail functionality

--- backend/db/models/user.model.js
+++ backend/db/models/user.model.js
@@ -4,12 +4,26 @@ const queries = require("../queries");
 async function add(name, email, password, avatar) {
   await new Promise((resolve, reject) => {
     mysql.query(queries.INSERT_USER_RECORD_SQL, [[[name, email, password, avatar]]], (err) => {
-      if (err) reject();
+      if (err) reject(err);
       else resolve();
     });
   });
 }
 
+
+async function getUserByEmail(email) {
+  const user = await new Promise((resolve, reject) => {
+    mysql.query(queries.SELECT_USER_BY_EMAIL_SQL, email, (err, data) => {
+      if (err) reject(err);
+      else resolve(data[0]);
+    });
+  });
+  return user;
+}
+
 module.exports = {
-  add
+  add,
+  getUserByEmail
 };
+
+
