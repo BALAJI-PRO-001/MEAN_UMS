79063dc7cc00e6fe9ee31974cedd986bb85103d4
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-17 21:08:33 +0530

    Pending: sign in functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,48 @@
+const bcryptjs = require("bcryptjs");
 const User = require("../db/models/user.model");
+const errorHandler = require("../utils/errorHandler");
 
-async function signUp(req, res) {
+async function signUp(req, res, next) {
   try {
-    const {name, email, password} = req.body;
-    await User.add(name, email, password, "some url");
+    const {name, email, password, avatar} = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    await User.add(name, email, hashedPassword, avatar);
+    res.status(201).json({
+      success: true,
+      message: "New user created"
+    });
   } catch(err) {
-    console.log(err);
+    next(errorHandler(409, "Duplicate Key"));
+  }
+}
+
+
+async function signIn(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.getUserByEmail(email);
+    
+    if (!user) {
+      return res.status(404).json({    
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    if (bcryptjs.compareSync(password, user.password)) {
+      
+    } else {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized"
+      });
+    }
+  } catch(err) {
+    next(err);
   }
 }
 
 module.exports = {
-  signUp
+  signUp,
+  signIn
 };
