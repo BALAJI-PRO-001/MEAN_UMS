6734cc985625c7eaf4112aafba12b523a0407c98
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-17 21:10:21 +0530

    Feat: new middleware for error handing

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,15 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server is running on port 3000!"));
