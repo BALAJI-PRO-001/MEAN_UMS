66f0f7119a06c9167a51083c239675fe851d6cf3
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-18 21:47:09 +0530

    Feat: user router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.route");
+const userRouter = require("./routes/user.route");
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
