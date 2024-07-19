0ad2868fd31f4173d76fae716563cf1980d13ce9
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-19 21:12:02 +0530

    Chor: complete

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,6 +1,26 @@
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
-  console.log(req.cookies);
+  const accessToken = req.cookies.access_token;
+  
+  if (!accessToken) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized"
+    });
+  }
+
+  jwt.verify(accessToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden"
+      });
+    } 
+    req.verifiedUserId = decoded.id;
+    next();
+  });
 }
 
 module.exports = verifyToken;
