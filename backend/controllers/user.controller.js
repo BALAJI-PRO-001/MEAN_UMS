25e499cddba2513613ca3c5e7b1dbe860fb7f2f8
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-19 21:12:29 +0530

    Pending: update functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,15 @@
 const User = require("../db/models/user.model");
+const errorHandler = require("../utils/errorHandler");
 
 async function updateUser(req, res, next) {
+  if (req.verifiedUserId != req.params.id) {
+    return next(errorHandler(401, "Unauthorized"));
+  }
+
+  if (req.body.id) {
+    return next(errorHandler(400, "Cannot update id"));
+  }
+
   // const userToUpdate = await User.getUserById(req.params.id);
   // console.log(userToUpdate);
 }
