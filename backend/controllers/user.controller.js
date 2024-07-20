c5d7dc625512f36013be36bc1313a9b83a40696a
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-20 21:08:06 +0530

    Feat: update and delete functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,19 +1,49 @@
+const bcryptjs = require("bcryptjs");
 const User = require("../db/models/user.model");
 const errorHandler = require("../utils/errorHandler");
 
 async function updateUser(req, res, next) {
-  if (req.verifiedUserId != req.params.id) {
-    return next(errorHandler(401, "Unauthorized"));
-  }
+  if (req.verifiedUserId != req.params.id) return next(errorHandler(401, "Unauthorized"));
+  if (req.body.id) return next(errorHandler(400, "Cannot update id"));
 
-  if (req.body.id) {
-    return next(errorHandler(400, "Cannot update id"));
-  }
+  try {
+    const userToUpdate = await User.getUserById(req.params.id);
+    if (!userToUpdate) return next(errorHandler(404, "User not found"));
+    if (req.body.password) req.body.password = bcryptjs.hashSync(req.body.password, 10);
 
-  // const userToUpdate = await User.getUserById(req.params.id);
-  // console.log(userToUpdate);
+    const updatedUser = await User.updateUser(req.params.id, req.body);
+    const { password, ...rest } = updatedUser;
+    res.status(200).json({
+      success: true,
+      data: {
+        user: rest
+      }
+    });
+  } catch(err) {
+    next(errorHandler(409, "Duplicate Key"));
+  }
 }
 
+
+async function deleteUser(req, res, next) {
+  if (req.verifiedUserId != req.params.id) return next(errorHandler(401, "Unauthorized"));
+
+  try {
+    const { email, password } = req.body;
+    const userToDelete = await User.getUserByEmail(email);
+    if (!userToDelete) return next(errorHandler(404, "User not found"));
+    const isvalidPassword = bcryptjs.compareSync(password, userToDelete.password);
+    if (!isvalidPassword) return next(errorHandler(401, "Unauthorized"));
+
+    await User.deleteUser(req.params.id);
+    res.status(204).json({});
+  } catch(err) {
+    next(err);
+  }
+}
+
+
 module.exports = {
-  updateUser
+  updateUser,
+  deleteUser
 };
