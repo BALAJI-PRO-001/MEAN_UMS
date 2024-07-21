a69ae721b76c217ee80e4949bfa6d9b0fa5dac5a
Balaji <160224234+BALAJI-PRO-001@users.noreply.github.com>
2024-07-21 22:01:16 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const { updateUser, deleteUser } = require("../controllers/user.controller");
 const verifyToken = require("../utils/verifyToken");
 const router = express.Router();
 
 router.patch("/update/:id", verifyToken, updateUser)
-      .delete("/delete/:id", verifyToken, deleteUser);
+      .delete("/delete/:id", verifyToken, deleteUser)
+      .post("/upload/avatar", uploadAvatar)
+      .get("/download/avatar/:fileName", downloadAvatar);
 
 module.exports = router;
 
 /*
   * PATCH: http://localhost:3000/api/v1/user/update/1
   * DELETE: http://localhost:3000/api/v1/user/delete/1
+  * POST: http://localhost:3000/api/v1/user/upload/avatar
+  * GET: http://localhost:3000/api/v1/user/download/avatar/1721574423851-Img-1.jpg
 */
