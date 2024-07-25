b82e56565b16f4b3d76707edc223934a7ff3ef4c
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-25 20:33:18 +0530

    Feat: signUpPage, signInPage functionality

--- backend/controllers/page.controller.js
+++ backend/controllers/page.controller.js
@@ -1,14 +1,30 @@
 const path = require("path");
 const fs = require("fs");
 
+const HEADER_COMPONENT_PATH = path.join(__dirname, "../../client/views/components/header.component.html");
 const INDEX_PAGE_PATH = path.join(__dirname, "../../client/views/index.html");
+const SIGN_UP_PAGE_PATH = path.join(__dirname, "../../client/views/pages/sign-up.html");
+const SIGN_IN_PAGE_PATH = path.join(__dirname, "../../client/views/pages/sign-in.html");
 
+const headerHTML = fs.readFileSync(HEADER_COMPONENT_PATH, "utf-8");
 const indexHTML = fs.readFileSync(INDEX_PAGE_PATH, "utf-8");
+const signUpHTML = fs.readFileSync(SIGN_UP_PAGE_PATH, "utf-8");
+const signInHTML = fs.readFileSync(SIGN_IN_PAGE_PATH, "utf-8");
 
 function indexPage(req, res) {
-  res.end(indexHTML);
+  res.end(indexHTML.replace("{{HEADER}}", headerHTML));
+}
+
+function signUpPage(req, res) {
+  res.end(signUpHTML.replace("{{HEADER}}", headerHTML));
+}
+
+function signInPage(req, res) {
+  res.end(signInHTML.replace("{{HEADER}}", headerHTML));
 }
 
 module.exports = {
-  indexPage
+  indexPage,
+  signUpPage,
+  signInPage
 };
