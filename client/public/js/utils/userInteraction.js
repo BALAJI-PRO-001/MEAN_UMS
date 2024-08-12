75a8ae58502140e62caefe050a6dcdb64bda282e
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-12 21:10:40 +0530

    Chor: No commit message

--- client/public/js/utils/userInteraction.js
+++ client/public/js/utils/userInteraction.js
@@ -1,6 +1,23 @@
 
-function toggleIcon(imgElement, oldPath, newPath) {
+export function toggleIcon(imgElement, oldPath, newPath) {
   if (imgElement == undefined || imgElement == null) {
-    throw new Error("Given ");
+    throw new Error("Given imgElement is null or undefined");
   }
+
+  if (imgElement.src.match(oldPath))
+    imgElement.src = newPath;
+  else 
+    imgElement.src = oldPath;
+}
+
+
+export function toggleType(element, oldType, newType) {
+  if (element == undefined || element == null) {
+    throw new Error("Given element is null or undefined");
+  }
+
+  if (element.type.match(oldType))
+    element.type = newType;
+  else 
+    element.type = oldType;
 }
