e2a695a3ec803ad5e3e88997f0fd481c36bef696
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-14 21:03:49 +0530

    Feat: validateNameAndUpdateUI  functionality

--- client/public/js/utils/userInteraction.js
+++ client/public/js/utils/userInteraction.js
@@ -1,3 +1,4 @@
+import validator from "./Validator.js";
 
 export function toggleIcon(imgElement, oldPath, newPath) {
   if (imgElement == undefined || imgElement == null) {
@@ -20,4 +21,35 @@ export function toggleType(element, oldType, newType) {
     element.type = newType;
   else 
     element.type = oldType;
+}
+
+
+export function setMessage(element, message) {
+  if (element == null || element == undefined) {
+    throw new Error("Given element is null or undefined");
+  }
+  element.innerText = message;
+}
+
+export function setBorder(element, cssValue) {
+  if (element == null || element == undefined) {
+    throw new Error("Given element is null or undefined");
+  }
+  element.style.border = cssValue;
+}
+
+export function validateNameAndUpdateUI(e) {
+  const name = e.target.value;
+  const { isValid, message } = validator.isValidName(name);
+  const errorElement = e.target.parentElement.querySelector("#error-element");
+
+  if (!isValid) {
+    setBorder(e.target, "2px solid red");
+    setMessage(errorElement, `${message} . . . .`)
+    return false;
+  } 
+
+  setBorder(e.target, "2px solid grey");
+  setMessage(errorElement, "");
+  return true;
 }
