576580dad2b5b51a35446f777b1d0c77e1fe8605
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-14 21:04:03 +0530

    Chor: No commit message

---
+++ client/public/js/utils/common.js
@@ -0,0 +1,14 @@
+
+export function addValidationListeners(inputElement, validationFunction) {
+  if (
+    inputElement == null || 
+    inputElement == undefined ||
+    validationFunction == null ||
+    validationFunction == undefined
+  ) {
+    throw new Error("Given inputElement or function is undefined or null");
+  }
+
+  inputElement.addEventListener("keyup", validationFunction);
+  inputElement.addEventListener("change", validationFunction);
+}
